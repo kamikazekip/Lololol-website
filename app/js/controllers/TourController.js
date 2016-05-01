c99b6e438e955c339dc4be56dde5b2fd52025ea8
kamikazekip <erik.brandsma@outlook.com>
2016-05-02 00:48:27 +0200

    Redesign

--- app/js/controllers/TourController.js
+++ app/js/controllers/TourController.js
@@ -2,7 +2,8 @@ module.exports = function($scope, $state, $lololol, $timeout, $window, $sce) {
 	var self = $scope;
 	var videoDiv = "#mainVPdiv";
 	var tourbar_point_id = "#tourbar-point-";
-	var thumbnailAnimationDuration = 250;
+	var thumbnailAnimationDuration = 300;
+	var thumbnailAnimationEasing = "easeInOutQuart"
 
 	$scope.videoPaused = false;
 	$scope.youtube_player;
@@ -12,73 +13,77 @@ module.exports = function($scope, $state, $lololol, $timeout, $window, $sce) {
 								iv_load_policy: 3
 							};
 
-	$scope.videos = [{
-		id: "1",
-		title: "Late for meeting",
-		youtube_embed: "wBqM2ytqHY4"
-	},
-	{
-		id: "2",
-		title: "Going to the store",
-		youtube_embed: "iRZ2Sh5-XuM"
-	},
-	{
-		id: "3",
-		title: "La la la cocaine",
-		youtube_embed: "zGX0LVkLr04"
-	},
-	{
-		id: "5",
-		title: "Wingardium leviosa",
-		youtube_embed: "FWtO0cfgewY"
-	}, 
-	{
-		id: "6",
-		title: "Wingardium leviosa 2",
-		youtube_embed: "reop2bXiNgk"
-	}, 
-	{
-		id: "7",
-		title: "Thor 'n' loki",
-		youtube_embed: "-CNlNYIRw4g"
-	},
-	{
-		id: "8",
-		title: "Dragonzball P (Dragonball Z Parody) - Oney Cartoons",
-		youtube_embed: "OYa5aQb3YGE"
-	},
-	{
-		id: "9",
-		title: "Dragonzball PeePee (Dragonball Z Parody Animation) - Oney Cartoons",
-		youtube_embed: "7pSmhZFbCy0"
-	},
-	{
-		id: "10",
-		title: "Henry the Hoover's Cocaine Overdose",
-		youtube_embed: "CmC62Eg82E8"
-	}, 
-	{
-		id: "11",
-		title: "DJ RAVINE'S BEST LAUNCHPAD COVER EVER - AVICII - LEVELS",
-		youtube_embed: "ya7FbdL5Xrk"
-	}, 
-	{
-		id: "12",
-		title: "You're a wizard harry!",
-		youtube_embed: "tKNhPpUR0Pg"
-	}, 
-	{
-		id: "13",
-		title: "Odin eating like a person",
-		youtube_embed: "4Ri5cszSKEg"
-	}, 
-	{
-		id: "14",
-		title: "Hey ron, hey billy", 
-		youtube_embed: "zBJU9ndpH1Q"
-	}];
-
-	$scope.currentVideo = $scope.videos[0];
+    $scope.tour = {
+    	title: "YouTube part 1",
+    	videos: [{
+			id: "1",
+			title: "Late for meeting",
+			embed: "wBqM2ytqHY4"
+		},
+		{
+			id: "2",
+			title: "Going to the store",
+			embed: "iRZ2Sh5-XuM"
+		},
+		{
+			id: "3",
+			title: "La la la cocaine",
+			embed: "zGX0LVkLr04"
+		},
+		{
+			id: "5",
+			title: "Wingardium leviosa",
+			embed: "FWtO0cfgewY"
+		}, 
+		{
+			id: "6",
+			title: "Wingardium leviosa 2",
+			embed: "reop2bXiNgk"
+		}, 
+		{
+			id: "7",
+			title: "Thor 'n' loki",
+			embed: "-CNlNYIRw4g"
+		},
+		{
+			id: "8",
+			title: "Dragonzball P (Dragonball Z Parody) - Oney Cartoons",
+			embed: "OYa5aQb3YGE"
+		},
+		{
+			id: "9",
+			title: "Dragonzball PeePee (Dragonball Z Parody Animation) - Oney Cartoons",
+			embed: "7pSmhZFbCy0"
+		},
+		{
+			id: "10",
+			title: "Henry the Hoover's Cocaine Overdose",
+			embed: "CmC62Eg82E8"
+		}, 
+		{
+			id: "11",
+			title: "DJ RAVINE'S BEST LAUNCHPAD COVER EVER - AVICII - LEVELS",
+			embed: "ya7FbdL5Xrk"
+		}, 
+		{
+			id: "12",
+			title: "You're a wizard harry!",
+			embed: "tKNhPpUR0Pg"
+		}, 
+		{
+			id: "13",
+			title: "Odin eating like a person",
+			embed: "4Ri5cszSKEg"
+		}, 
+		{
+			id: "14",
+			title: "Hey ron, hey billy", 
+			embed: "zBJU9ndpH1Q"
+		}]
+    }
+	$scope.currentTour = $scope.tour; 
+	$scope.videos = $scope.currentTour.videos;
+	$scope.currentVideo = $scope.currentTour.videos[0];
 
 	$scope.select_video = function(video){
 		$scope.youtube_options.autoplay = 1;
@@ -92,20 +97,20 @@ module.exports = function($scope, $state, $lololol, $timeout, $window, $sce) {
 
 	$scope.tourbar_point_hover = function(video){
 		$("#videothumbtitle").html(video.title);
-		$("#videothumbnail").attr("src", "http://img.youtube.com/vi/" + video.youtube_embed + "/0.jpg");
+		$("#videothumbnail").attr("src", "http://img.youtube.com/vi/" + video.embed + "/0.jpg");
 		var tourbar_point 	= $(tourbar_point_id + video.id);
 		var thumbnail 		= $("#videothumb");
-		var top 			= tourbar_point.offset().top - thumbnail.height() - 50;
-		var left 			= tourbar_point.offset().left + ( tourbar_point.width() / 2 ) - thumbnail.width() / 2;
+		var top 			= tourbar_point.offset().top + 50;
+		var left 			= tourbar_point.offset().left - ( tourbar_point.width() / 2 ) - thumbnail.width() / 2 + 30;
 		thumbnail.stop();
-		thumbnail.css({"top" : top + "px", "left": left + "px"});
-		thumbnail.animate({ opacity: 1, top: "-=10px" }, thumbnailAnimationDuration);
+		thumbnail.css({"opacity": 0, "top": top + "px", "left": left + "px"});
+		thumbnail.animate({ opacity: 1, top: "+=30px" }, thumbnailAnimationDuration, thumbnailAnimationEasing);
 	}
 
 	$scope.tourbar_point_hover_out = function(video){
 		var thumbnail 		= $("#videothumb");
 		thumbnail.stop();
-		thumbnail.animate({ opacity: 0, top: "+=30px" }, thumbnailAnimationDuration, function(){
+		thumbnail.animate({ opacity: 0, top: "+=30px" }, thumbnailAnimationDuration, thumbnailAnimationEasing, function(){
 			var top 	= 0 - thumbnail.height() - 10;
 			var left 	= 0 - thumbnail.width()  - 10;
 			thumbnail.css({"top": top + "px", "left": left + "px"});
@@ -139,7 +144,6 @@ module.exports = function($scope, $state, $lololol, $timeout, $window, $sce) {
 
 	$scope.positionDots = function(){
 		$('#tourbar-actual').children('.tourbar-point').each(function (index) {
-			console.log("HALLO");
 			leftPercentage = index * (100 / ($scope.videos.length - 1));
 			var width = $(this).width();
 			var parentWidth = $(this).parent().width();
